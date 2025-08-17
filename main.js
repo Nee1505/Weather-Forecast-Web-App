3d36359f1e03901289aeb0baded48249c3325877
Neelam <meritneelam2004@gmail.com>
2025-08-17 09:25:00 +0000

    feat: display dynamic weather icons based on current conditions

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ const apiKey = "39eecc358e9f9eb8e50574f4473f1e56";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
 
 
+const searchBox = document.querySelector(".search input"); 
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+});
+
+
 async function checkWeather(city) {
     const response = await fetch(apiUrl + "&q=" +city + `&appid=${apiKey}`); 
     let data = await response.json();
@@ -12,11 +21,22 @@ async function checkWeather(city) {
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity+ "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " kmph";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+    
+
 }
-
-const searchBox = document.querySelector(".search input"); 
-const searchBtn = document.querySelector(".search button");
-
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
-})
